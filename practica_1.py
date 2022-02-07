9ac7252c9ecc2b5a7bd97de0b9121a1d85d07ba5
JRobayo99 <jramire23664@universidadean.edu.co>
2022-02-07 16:06:07 -0500

    Nuevo

---
+++ practica_1.py
@@ -0,0 +1,32 @@
+num1=int(input("introduce el valor en pesos: "))
+"""print ("Introduce el número: ")
+numero = int(input ())
+
+umil = numero / 1000
+tmp = numero % 1000
+
+centenas = tmp / 100
+tmp = tmp % 100
+
+decenas = tmp / 10
+unidades = tmp % 10
+
+print ("Unidades de millar: %i" % umil)
+print ("Centenas: %i" % centenas)
+print ("Decenas: %i" % decenas)
+print ("Unidades: %i" % unidades)"""
+
+if(num1>=0 and num1<1000):
+    num2= num1//100
+    tmp= num1%100
+    if(tmp>=0 and tmp<100):
+
+        num3=tmp//10
+
+
+
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+print(num6)
